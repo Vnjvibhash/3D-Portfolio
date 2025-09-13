01d7258cf6292755375f7c52828c176179044f21
Vivek Kumar <vnjvibhash@gmail.com>
2025-09-14 00:26:12 +0530

    Experience Updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -236,8 +236,6 @@ export const myProjects = [
   }
 ];
 
-
-
 export const mySocials = [
   {
     name: "WhatsApp",
@@ -258,27 +256,15 @@ export const mySocials = [
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
     contents: [
-      "✅ Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "✅ Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "✅ Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "✅ Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "✅ Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
-    ],
-  },
-  {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
-    contents: [
-      "✅ Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "✅ Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control.",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+      "✅ Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "✅ Built custom mobile and web applications for clients, including e-commerce, utility, and productivity solutions.",
+      "✅ Designed Play Store-ready app screenshots and marketing assets for clients using Figma, Photoshop, and Flutter-based tools.",
+      "✅ Integrated third-party APIs and optimized back-end workflows for high performance.",
+      "✅ Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies."
     ],
   },
   {
@@ -294,15 +280,27 @@ export const experiences = [
     ],
   },
   {
-    title: "Freelance Developer",
-    job: "Self-Employed",
-    date: "2025-Present",
+    title: "Back-End Developer",
+    job: "Car Manufacture",
+    date: "2023-2024",
     contents: [
-      "✅ Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "✅ Built custom mobile and web applications for clients, including e-commerce, utility, and productivity solutions.",
-      "✅ Designed Play Store-ready app screenshots and marketing assets for clients using Figma, Photoshop, and Flutter-based tools.",
-      "✅ Integrated third-party APIs and optimized back-end workflows for high performance.",
-      "✅ Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies."
+      "✅ Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+      "✅ Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control.",
+      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    ],
+  },
+  {
+    title: "Software Developer",
+    job: "Security & Defense Projects",
+    date: "2021-2023",
+    contents: [
+      "✅ Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "✅ Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "✅ Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "✅ Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "✅ Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
     ],
   },
 ];
