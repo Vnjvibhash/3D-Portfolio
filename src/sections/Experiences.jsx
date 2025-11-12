e7919f7de67f2f44b38b30fd8519e3ede869a68f
Vivek Kumar <vnjvibhash@gmail.com>
2025-11-12 22:53:12 +0530

    Navigation Updated

--- src/sections/Experiences.jsx
+++ src/sections/Experiences.jsx
@@ -2,7 +2,7 @@ import { Timeline } from "../components/Timeline";
 import { experiences } from "../constants";
 const Experiences = () => {
   return (
-    <div className="w-full">
+    <div className="w-full" id="experiences">
       <Timeline data={experiences} />
     </div>
   );
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -19,6 +19,11 @@ function Navigation() {
         </a>
       </li>
       <li className="nav-li">
+        <a className="nav-link" href="#experiences">
+          Experiences
+        </a>
+      </li>
+      <li className="nav-li">
         <a className="nav-link" href="#contact">
           Contact
         </a>
